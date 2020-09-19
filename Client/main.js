cb82177e37569db260ef56d822cc5ae5f7f5c744
TFTWPhoenix <chucktcg12@gmail.com>
2020-09-18 23:51:59 -0500

    Add test authrequest

--- Client/main.js
+++ Client/main.js
@@ -1,15 +1,18 @@
-function authRequest(username, attemptedToken, authServer) {
+const http = require("http");
+const { Http2ServerRequest } = require("http2");
+
+function authRequest(username, attemptedToken) {
 
     // This will send an authentication request attempting to authenticate username with the token attemptedToken using authServer.
+    http.get("http://apmunute.me/testSession.php?username=" + username + "&attemptedToken=" + attemptedToken, function(message) {
+
+
+
+    });
 
 }
-function deauthRequest(username, token, authServer) {
+function deauthRequest(username, token) {
 
     // Deauthorizes (logs out) username with the token token using authServer.
-
-}
-function pingServer(authServer) {
-
-    // This will ping authServer to make sure it is online.
 
 }
