fa7cda228120acd617e8bc209440240868963972
TFTWPhoenix <chucktcg12@gmail.com>
2020-09-18 23:52:57 -0500

    Same as last commit. It didn't fully commit.

--- Client/main.js
+++ Client/main.js
@@ -4,9 +4,9 @@ const { Http2ServerRequest } = require("http2");
 function authRequest(username, attemptedToken) {
 
     // This will send an authentication request attempting to authenticate username with the token attemptedToken using authServer.
-    http.get("http://apmunute.me/testSession.php?username=" + username + "&attemptedToken=" + attemptedToken, function(message) {
+    http.get("http://apmunute.me/testSession.php?username=" + username + "&attemptedToken=" + attemptedToken, function(response) {
 
-
+        console.log(response);
 
     });
 
