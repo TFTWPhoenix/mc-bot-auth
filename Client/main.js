70af7024443731fa59b3ca620529e48755d7f7f4
TFTWPhoenix <chucktcg12@gmail.com>
2020-09-18 22:52:49 -0500

    Add stub main.js for api.

---
+++ Client/main.js
@@ -0,0 +1,15 @@
+function authRequest(username, attemptedToken, authServer) {
+
+    // This will send an authentication request attempting to authenticate username with the token attemptedToken using authServer.
+
+}
+function deauthRequest(username, token, authServer) {
+
+    // Deauthorizes (logs out) username with the token token using authServer.
+
+}
+function pingServer(authServer) {
+
+    // This will ping authServer to make sure it is online.
+
+}
